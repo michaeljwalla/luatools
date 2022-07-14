390c71e087a68e6a829a4aebe1174ec206d2d150
michaeljwalla <109261155+michaeljwalla@users.noreply.github.com>
2022-07-13 23:17:39 -0500

    Update whitelist.lua

--- whitelist.lua
+++ whitelist.lua
@@ -1,1 +1,17 @@
-return {}
+local lp = game.Players.LocalPlayer
+local wl = {
+  {
+    UserId = 3312237316,
+    HWID = '8DAEF9D0-637F-4264-AAE3-03E5F8725CFD'
+    Dev = true
+  }
+}
+local whitelisted, dev
+for i,v in pairs(wl) do
+    if v.UserId == lp.UserId or v.HWID == gethwid() then
+    if v.Dev then dev = true end
+    whitelisted = true 
+    break
+  end
+end
+return whitelisted, (dev and wl) or nil
