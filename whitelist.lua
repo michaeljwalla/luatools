31e356701888bc2013cb7ada9732e1b2049d273e
michaeljwalla <109261155+michaeljwalla@users.noreply.github.com>
2022-07-13 23:22:42 -0500

    Update whitelist.lua

--- whitelist.lua
+++ whitelist.lua
@@ -1,16 +1,19 @@
 local lp = game.Players.LocalPlayer
+function gethwid()
+    return game:GetService("RbxAnalyticsService"):GetClientId()
+end
 local wl = {
   {
     UserId = 3312237316,
-    HWID = {
+     HWID = {
       ['8DAEF9D0-637F-4264-AAE3-03E5F8725CFD'] = true
-    }
-    Dev = true
+    },
+    Dev = false
   }
 }
 local whitelisted, dev
 for i,v in pairs(wl) do
-    if v.UserId == lp.UserId or v.HWID == gethwid() then
+    if v.UserId == lp.UserId and v.HWID[tostring(gethwid())] then
     if v.Dev then dev = true end
     whitelisted = true 
     break
