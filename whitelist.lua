e6022ef3641347555cf4b9d26a8f801d14773ff4
michaeljwalla <109261155+michaeljwalla@users.noreply.github.com>
2022-07-14 00:07:49 -0500

    Update whitelist.lua

--- whitelist.lua
+++ whitelist.lua
@@ -11,15 +11,19 @@ local wl = {
     Dev = true
   }
 }
-local whitelisted, dev
-for i,v in pairs(wl) do
-    if v.HWID[tostring(gethwid())] and (v.Dev or v.UserId == lp.UserId) then
-    if v.Dev then dev = true  end
-    whitelisted = true 
-    break
-  end
+
+function iswl()
+    local id, hwid, dev = false, false, false
+    for i,v in pairs(wl) do
+        if v.UserId == lp.UserId then id = true end
+        if v.HWID == gethwid() then hwid = true end
+        if v.Dev then dev = true end
+      end
+    end
+    return id, hwid, dev
 end
-function getwl()
+function getwltable()
     if dev then return wl else do lp:Kick("You are not a dev") end end
 end
-return {Whitelisted = whitelisted, Dev = dev, GetWhitelist = getwl}
+local id, hwid, dev = iswl()
+return {Whitelisted = id or hwid, Dev = dev, GetWhitelist = getwltable, WhitelistedBy = {UserId = id, HWID = hwid, Dev = dev}}
