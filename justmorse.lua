4608e48887bf3ee090da140206c722bb3027f1cb
michaeljwalla <109261155+michaeljwalla@users.noreply.github.com>
2023-01-19 12:32:07 -0600

    Update justmorse.lua

--- justmorse.lua
+++ justmorse.lua
@@ -148,11 +148,13 @@ local tableencode,tabledecode = (function()
             if a1 or a2 then
                 if a2 then scope = scope + 1 end
                 spillover = spillover..(a1 and ch(types.c) or "")..i
-                if not i:reverse():find("^"..ch(types.ee)) then continue end
-                scope = scope - 1
-                if (scope ~= 0) then continue end
-                i = spillover
-                spillover = ""
+                if i:reverse():find("^"..ch(types.ee)) then
+                    scope = scope - 1
+                    if (scope == 0) then
+                        i = spillover
+                        spillover = ""
+                    end
+                end
             end
             table.insert(x, i)
         end
@@ -173,10 +175,10 @@ local tableencode,tabledecode = (function()
             if rawequal(type, "n") then
                 local _start, _end = data:find(ch(types.k))
                 assert(_start, "Invalid key.")
-                local key = mc.Decode(data:sub(1, _end-1))
+                local key = parse(data:sub(1, _end-1))
                 local ending = data:find(ch(types.ea))
                 assert(ending, "Read left open for key: "..key)
-                local value = tonumber(mc.Decode(data:sub(_end+1, ending - 1)))
+                local value = tonumber(parse(data:sub(_end+1, ending - 1)))
                 assert(value, "Invalid double for key: "..key)
 
                 new[
@@ -185,26 +187,27 @@ local tableencode,tabledecode = (function()
             elseif rawequal(type, "s") then
                 local _start, _end = data:find(ch(types.k))
                 assert(_start, "Invalid key.")
-                local key = mc.Decode(data:sub(1, _end-1))
+                local key = parse(data:sub(1, _end-1))
                 local ending = data:find(ch(types.ea))
                 assert(ending, "Read left open for key: "..key)
-                local value = mc.Decode(data:sub(_end+1, ending - 1))
+                local value = parse(data:sub(_end+1, ending - 1))
                 new[
                     tonumber(key) and tonumber(key) or key
                     ] = value
             elseif rawequal(type, "e") then
                 local _start, _end = data:find(ch(types.k))
                 assert(_start, "Invalid key.")
-                local key = mc.Decode(data:sub(1, _end-1))
+                local key = parse(data:sub(1, _end-1))
                 local ending = data:reverse():find(ch(types.ee)) --last index of closetbl
                 assert(ending, "Unclosed table for key: "..key)
                 ending = #data - ending
                 local newtblstr = data:sub(_end+1, ending)
-                if #newtblstr == 0 then continue end
-                local value = decode(newtblstr, true)
-                new[
-                    tonumber(key) and tonumber(key) or key
-                    ] = value
+                if #newtblstr ~= 0 then
+                    local value = decode(newtblstr, true)
+                    new[
+                        tonumber(key) and tonumber(key) or key
+                        ] = value
+                end
             else
                 error"Invalid decode format."
             end
@@ -217,19 +220,18 @@ local tableencode,tabledecode = (function()
         local str = ""
         local ctr = 0
         for i,v in next, tbl do
-            --if type(i) ~= "string" then --[[warn"Keys force as strings."]] end
             if ctr > 0 then str = str..ch(types.c) end
             local type = type(v)
             type = (type == 'number' and 'n') or (type == 'string' and 's') or (type == 'table' and 'e') or error"unsupported type: "..type
             if type ~= 'e' then
                 str = str..ch(types[type])
-                ..mc.Encode(i)..ch(types.k)
-                ..mc.Encode(v)..ch(types.ea)--..ch(types.c)
+                ..to(i)..ch(types.k)
+                ..to(v)..ch(types.ea)--..ch(types.c)
             else
-                assert(not table.find(nocycles, v), "Cyclic table detected.")
+                assert(not tf(nocycles, v), "Cyclic table detected.")
                 local data = encode(v, nocycles, recursed)
                 table.insert(nocycles, v) --no cyclic detection = stack overflow
-                str = str..ch(types[type])..mc.Encode(i)..ch(types.k)
+                str = str..ch(types[type])..to(i)..ch(types.k)
                 ..data..ch(types.ee)
             end
     --append to old:   [datatype],      [keyname],   [key-value separator], [value], [END OF READ], [comma]
@@ -240,15 +242,8 @@ local tableencode,tabledecode = (function()
     return encode, decode
 end)()
 local module = {
-    Decode = function(a)
-        local ax, ay = pcall(parse, a)
-        local x,y = (ax and ay) or pcall(tabledecode, a)
-        return type(a) == 'table' and tabledecode(a) or parse(a)
-    end,
-    Encode = function(a)
-        local ax, ay = pcall(to, a)
-        local x,y = (ax and ay) or pcall(tableencode, a)
-        return assert(x, "Unknown") and (x and y)
-    end
+    DecodeText = parse,
+    EncodeText = to,
+    EncodeTable = tableencode,
+    DecodeTable = tabledecode
 }
-return module, info
