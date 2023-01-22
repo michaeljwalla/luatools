8a8f35902fc31f631545a8d742f99fea5608f9b1
michaeljwalla <109261155+michaeljwalla@users.noreply.github.com>
2023-01-21 18:26:12 -0600

    Update justmorse.lua

--- justmorse.lua
+++ justmorse.lua
@@ -77,6 +77,25 @@ local tree = {
     32
 }
 local arctree = {[45] = 12123, [95] = 12112, [38] = 12111, [58] = 11221, [46] = 11121, [32] = 3,[69] = 13,[84] = 23,[73] = 113,[65] = 123,[78] = 213,[77] = 223,[83] = 1113,[85] = 1123,[82] = 1213,[87] = 1223,[68] = 2113,[75] = 2123,[71] = 2213,[79] = 2223,[72] = 11113,[86] = 11123,[70] = 11213,[76] = 12113,[80] = 12213,[74] = 12223,[66] = 21113,[88] = 21123,[67] = 21213,[89] = 21223,[90] = 22113,[81] = 22123,[53] = 11111,[52] = 11112,[51] = 11122,[50] = 11222,[43] = 12121,[49] = 12222,[54] = 21111,[61] = 21112,[47] = 21121,[55] = 22111,[56] = 22211,[57] = 22221,[48] = 22222}
+local comma = c(info.c)
+local bools = {
+    ['true'] = true,
+    ['false'] = false
+}
+local typetable = {
+    string = 's',
+    number = 'n',
+    table = 'e',
+    boolean = 'b'
+}
+local function find(tbl, val)
+    for i,v in next, tbl do if v == val then return i end end
+    return
+end
+local function isescape(str)
+    local ind = find(info, b(str))
+    return ind and type(ind) == 'string' and ind
+end
 local c, b = string.char, string.byte
 local function tf(tbl, item)
     for i,v in next, tbl do
@@ -84,7 +103,7 @@ local function tf(tbl, item)
     end
    return 
 end
-local function split (inputstr, sep)
+local function split(inputstr, sep)
         if sep == nil then
                 sep = "%s"
         end
@@ -103,20 +122,16 @@ end
 local function let(str)
     str = totalsplit(tostring(str))
     local res
-    pcall(function()
-        local st = tree
-        
-        
-        for i = 1, #str do
-            local s = tonumber(tf(info, b(str[i])))
-            local off = 0
-            if (s == 4) then s = s - 1 off = 32 end
-            st = st[s]
-            if (type(st) == 'number') then st = st + off end
-        end
-        assert(st and type(st) == 'number')
-        res = c(st)
-    end)
+    local st = tree
+    for i = 1, #str do
+        local s = tonumber(tf(info, b(str[i])))
+        local off = 0
+        if (s == 4) then s = s - 1 off = 32 end
+        st = st[s]
+        if (type(st) == 'number') then st = st + off end
+    end
+    assert(st and type(st) == 'number')
+    res = c(st)
     return res
 end
 local function parse(tbl, cc)
@@ -162,26 +177,7 @@ local tableencode,tabledecode = (function()
         s = 27, --string
         c = 4, --comma (table values)
     }]]
-    local ch, by = string.char, string.byte
-    local comma = ch(info.c)
-    local bools = {
-        ['true'] = true,
-        ['false'] = false
-    }
-    local typetable = {
-        string = 's',
-        number = 'n',
-        table = 'e',
-        boolean = 'b'
-    }
-    local function find(tbl, val)
-        for i,v in next, tbl do if v == val then return i end end
-        return
-    end
-    local function isescape(str)
-        local ind = find(info, by(str))
-        return ind and type(ind) == 'string' and ind
-    end
+    
     local function csplit(str)
         local x = {}
         local scope = 0
@@ -241,10 +237,10 @@ local tableencode,tabledecode = (function()
             data = data:sub(2,-1) --remove identifier
             
             if rawequal(type, "n") then
-                local _start, _end = data:find(ch(info.k))
+                local _start, _end = data:find(c(info.k))
                 assert(_start, "Invalid key.")
                 local key = parse(data:sub(1, _end-1))
-                local ending = data:find(ch(info.ea))
+                local ending = data:find(c(info.ea))
                 assert(ending, "Read left open for key: "..key)
                 local value = tonumber(parse(data:sub(_end+1, ending - 1)))
                 assert(value, "Invalid double for key: "..key)
@@ -252,10 +248,10 @@ local tableencode,tabledecode = (function()
                     tonumber(key) and tonumber(key) or key
                     ] = value
             elseif rawequal(type, "b") then
-                local _start, _end = data:find(ch(info.k))
+                local _start, _end = data:find(c(info.k))
                 assert(_start, "Invalid key.")
                 local key = parse(data:sub(1, _end-1))
-                local ending = data:find(ch(info.ea))
+                local ending = data:find(c(info.ea))
                 assert(ending, "Read left open for key: "..key)
                 local value = bools[parse(data:sub(_end+1, ending - 1))]
                 assert(value ~= nil, "Invalid boolean for key: "..key)
@@ -263,20 +259,20 @@ local tableencode,tabledecode = (function()
                     tonumber(key) and tonumber(key) or key
                     ] = value
             elseif rawequal(type, "s") then
-                local _start, _end = data:find(ch(info.k))
+                local _start, _end = data:find(c(info.k))
                 assert(_start, "Invalid key.")
                 local key = parse(data:sub(1, _end-1))
-                local ending = data:find(ch(info.ea))
+                local ending = data:find(c(info.ea))
                 assert(ending, "Read left open for key: "..key)
                 local value = parse(data:sub(_end+1, ending - 1))
                 new[
                     tonumber(key) and tonumber(key) or key
                     ] = value
             elseif rawequal(type, "e") then
-                local _start, _end = data:find(ch(info.k))
+                local _start, _end = data:find(c(info.k))
                 assert(_start, "Invalid key.")
                 local key = parse(data:sub(1, _end-1))
-                local ending = data:reverse():find(ch(info.ee)) --last index of closetbl
+                local ending = data:reverse():find(c(info.ee)) --last index of closetbl
                 assert(ending, "Unclosed table for key: "..key)
                 ending = #data - ending
                 local newtblstr = data:sub(_end+1, ending)
@@ -302,16 +298,16 @@ local tableencode,tabledecode = (function()
             local type = type(v)
             type = typetable[type] or error("unsupported type: "..type)
             if type ~= 'e' then
-                str = str..ch(info[type])
-                ..to(i)..ch(info.k)
-                ..to(v)..ch(info.ea)--..ch(info.c)
+                str = str..c(info[type])
+                ..to(i)..c(info.k)
+                ..to(v)..c(info.ea)--..c(info.c)
             else
                 --print()
                 assert(not tf(nocycles, v), "Cyclic table detected.")
                 
                 local data = encode(v, nocycles, recursed)
                 table.insert(nocycles, v) --no cyclic detection = stack overflow
-                local newentry = ch(info.e)..to(i)..ch(info.k)..(data or "")..ch(info.ee)
+                local newentry = c(info.e)..to(i)..c(info.k)..(data or "")..c(info.ee)
                 str = str..newentry
             end
     --append to old:   [datatype],      [keyname],   [key-value separator], [value], [END OF READ], [comma]
@@ -325,6 +321,18 @@ local module = {
     DecodeText = parse,
     EncodeText = to,
     EncodeTable = tableencode,
-    DecodeTable = tabledecode
+    DecodeTable = tabledecode,
+    FetchSave = function(path)
+        local a, save = isfile(path) and pcall(tabledecode, readfile(path))
+        if a then return save end
+        return 
+        -- some ppl like `if fetchsave() == nil`
+        -- instead of just `if not fetchsave()`
+        -- looks so ugly
+        -- `return a and save` on top...
+    end,
+    SaveTo = function(path, tbl)
+        return writefile(path, tableencode(tbl)) or path
+    end
 }
 return module
