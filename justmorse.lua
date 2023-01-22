d7350ed60d5b5a57c0df76a7fbcc01924a99d27f
michaeljwalla <109261155+michaeljwalla@users.noreply.github.com>
2023-01-21 18:07:17 -0600

    Update justmorse.lua

--- justmorse.lua
+++ justmorse.lua
@@ -3,14 +3,18 @@ local info = {
     [2] = 26,
     [3] = 7,
     [4] = 8,
-    e = 18, --entry
+    e = 18, --table
+    b = 15, --bool
     n = 30, --num
     s = 27, --string
-    ee = 22, --un-entry
+    ee = 22, --exit nested table read
+    ea = 23, --exit value-read (non table)
     u = 25, --lowercase,
     ne = 3, --next
-    ne2 = 4, --
+    k = 17, --key: before, value: after
+    c = 4, --comma (table values)
 }
+
 local tree = {
     {
         {
@@ -20,20 +24,59 @@ local tree = {
                 83
             },
             {
-                {nil, nil, 70},
-                {nil, 50, nil},
+                {37, 47, 70},
+                {58, 50, 43},
                 85
             },
             73
         },
         {
             {
-                {nil,nil,76},
-                {43,nil,nil},
+                {38,95,76},
+                {43,61,45},
                 82
             },
-            {{nil,nil,80}, {nil,49,74}, 87}, 65}, 69}, {{{{54,61,66}, {47,nil,88}, 68}, {{nil,nil,67}, {nil,nil,89}, 75}, 78},{{{55,nil,90}, {nil,nil,81}, 71}, {{56},{57,48},79}, 77}, 84}, 32}
-local arctree = {[46] = 11121, [32] = 3,[69] = 13,[84] = 23,[73] = 113,[65] = 123,[78] = 213,[77] = 223,[83] = 1113,[85] = 1123,[82] = 1213,[87] = 1223,[68] = 2113,[75] = 2123,[71] = 2213,[79] = 2223,[72] = 11113,[86] = 11123,[70] = 11213,[76] = 12113,[80] = 12213,[74] = 12223,[66] = 21113,[88] = 21123,[67] = 21213,[89] = 21223,[90] = 22113,[81] = 22123,[53] = 11111,[52] = 11112,[51] = 11122,[50] = 11222,[43] = 12121,[49] = 12222,[54] = 21111,[61] = 21112,[47] = 21121,[55] = 22111,[56] = 22211,[57] = 22221,[48] = 22222}
+            {
+                {nil,nil,80},
+                {nil,49,74},
+                87
+            },
+            65
+        },
+        69
+    },
+    {
+        {
+            {
+                {54,61,66},
+                {47,nil,88},
+                68
+            },
+            {
+                {nil,nil,67},
+                {nil,nil,89},
+                75
+            },
+            78
+        },
+        {
+            {
+                {55,nil,90},
+                {nil,nil,81},
+                71
+            },
+            {
+                {56,nil,nil},
+                {57,48,nil},
+                79
+            },
+            77
+        },
+        84
+    },
+    32
+}
+local arctree = {[45] = 12123, [95] = 12112, [38] = 12111, [58] = 11221, [46] = 11121, [32] = 3,[69] = 13,[84] = 23,[73] = 113,[65] = 123,[78] = 213,[77] = 223,[83] = 1113,[85] = 1123,[82] = 1213,[87] = 1223,[68] = 2113,[75] = 2123,[71] = 2213,[79] = 2223,[72] = 11113,[86] = 11123,[70] = 11213,[76] = 12113,[80] = 12213,[74] = 12223,[66] = 21113,[88] = 21123,[67] = 21213,[89] = 21223,[90] = 22113,[81] = 22123,[53] = 11111,[52] = 11112,[51] = 11122,[50] = 11222,[43] = 12121,[49] = 12222,[54] = 21111,[61] = 21112,[47] = 21121,[55] = 22111,[56] = 22211,[57] = 22221,[48] = 22222}
 local c, b = string.char, string.byte
 local function tf(tbl, item)
     for i,v in next, tbl do
@@ -110,7 +153,7 @@ local function to(str)
 end
 
 local tableencode,tabledecode = (function() 
-    local types = {
+    --[[local types = {
         k = 17, --key: before, value: after
         e = 18, --table
         ee = 22, --exit table scope
@@ -118,96 +161,131 @@ local tableencode,tabledecode = (function()
         n = 30, --num
         s = 27, --string
         c = 4, --comma (table values)
+    }]]
+    local ch, by = string.char, string.byte
+    local comma = ch(info.c)
+    local bools = {
+        ['true'] = true,
+        ['false'] = false
     }
-    local function split(str, splt)
-        local new = {}
-        for v in str:gmatch(("([^%s]*)"):format(splt)) do table.insert(new, v) end
-        return new
-    end
-    local function countscope(line)
-        if true then return 0, line end
-        local i = 0
-        line = line:gsub("\t", function() i = i + 1 return "" end)
-        return i, line
-    end
+    local typetable = {
+        string = 's',
+        number = 'n',
+        table = 'e',
+        boolean = 'b'
+    }
     local function find(tbl, val)
         for i,v in next, tbl do if v == val then return i end end
         return
     end
-    local ch, by = string.char, string.byte
     local function isescape(str)
-        local ind = find(types, by(str))
+        local ind = find(info, by(str))
         return ind and type(ind) == 'string' and ind
     end
     local function csplit(str)
         local x = {}
-        local spillover = ""
         local scope = 0
-        for i in str:gmatch("[^"..ch(types.c).."]+") do
-            local a1, a2 = #spillover > 0, i:find("^"..ch(types.e))
+        local start = str
+        while true do
+            local comma, nest = start:find(comma), start:find"^\18[\14\26\7\8]*\17"
+            if not comma then
+                if #start > 0 then table.insert(x, start) end
+                break
+            elseif not nest then
+                local value = start:sub(1,comma-1)
+                start = start:sub(comma+1)
+                if #value > 0 then table.insert(x, value) end
+            elseif nest < comma then
+                local chunk = start:sub(nest)
+                local value = ""
+                scope = 0 --always pushes to 1
+                for i = 1, #chunk do
+                    local char = b(chunk:sub(i,i))
+                    if char == info.e then scope = scope + 1
+                    elseif char == info.ee then scope = scope - 1 end
+                    value = value..c(char)
+                    if (scope == 0) then break end
+                end
+                assert(scope == 0, 'unequal table grouping.')
+                start = start:sub(nest + #value)
+                table.insert(x, value)
+            end
+        end
+        return x
+        --[[
+        for i in str:gmatch("[^"..comma.."]+") do --for nested tables
+            local a1, a2 = #spillover > 0, i:find()
             if a1 or a2 then
                 if a2 then scope = scope + 1 end
-                spillover = spillover..(a1 and ch(types.c) or "")..i
-                if i:reverse():find("^"..ch(types.ee)) then
+                spillover = spillover..(a1 and comma or "")..i
+                if i:reverse():find("^"..ch(info.ee)) then
                     scope = scope - 1
                     if (scope == 0) then
-                        i = spillover
+                        table.insert(x, spillover)
                         spillover = ""
                     end
                 end
+            else
+                table.insert(x, i)
             end
-            table.insert(x, i)
-        end
-        return x
+        end]]
+        
     end
-    local comma = ch(types.c)
     local function decode(str, recursed)
-        local str = type(str) == 'table' and str or csplit(str) --FOUND ISSUE: SPLITTING INSIDE OF NESTED TABLES!! FIX REGEX
-        --probably will need to write own.
+        local str = (type(str) == 'table' and str) or csplit(str:reverse():gsub("\n.*$", ""):reverse())
         local new = {}
-        inscope = inscope or {open = 1, closed = 0}
+        
         for i,v in next, str do
-            local scope, data = countscope(v)
-            assert(scope + inscope.open <= inscope.closed + 1, 'Invalid scope.')
-            --inscope.open = inscope.open + 1 --tables not supported yet
+            local data = v
             local type = isescape(data)
             data = data:sub(2,-1) --remove identifier
+            
             if rawequal(type, "n") then
-                local _start, _end = data:find(ch(types.k))
+                local _start, _end = data:find(ch(info.k))
                 assert(_start, "Invalid key.")
                 local key = parse(data:sub(1, _end-1))
-                local ending = data:find(ch(types.ea))
+                local ending = data:find(ch(info.ea))
                 assert(ending, "Read left open for key: "..key)
                 local value = tonumber(parse(data:sub(_end+1, ending - 1)))
                 assert(value, "Invalid double for key: "..key)
-
+                new[
+                    tonumber(key) and tonumber(key) or key
+                    ] = value
+            elseif rawequal(type, "b") then
+                local _start, _end = data:find(ch(info.k))
+                assert(_start, "Invalid key.")
+                local key = parse(data:sub(1, _end-1))
+                local ending = data:find(ch(info.ea))
+                assert(ending, "Read left open for key: "..key)
+                local value = bools[parse(data:sub(_end+1, ending - 1))]
+                assert(value ~= nil, "Invalid boolean for key: "..key)
                 new[
                     tonumber(key) and tonumber(key) or key
                     ] = value
             elseif rawequal(type, "s") then
-                local _start, _end = data:find(ch(types.k))
+                local _start, _end = data:find(ch(info.k))
                 assert(_start, "Invalid key.")
                 local key = parse(data:sub(1, _end-1))
-                local ending = data:find(ch(types.ea))
+                local ending = data:find(ch(info.ea))
                 assert(ending, "Read left open for key: "..key)
                 local value = parse(data:sub(_end+1, ending - 1))
                 new[
                     tonumber(key) and tonumber(key) or key
                     ] = value
             elseif rawequal(type, "e") then
-                local _start, _end = data:find(ch(types.k))
+                local _start, _end = data:find(ch(info.k))
                 assert(_start, "Invalid key.")
                 local key = parse(data:sub(1, _end-1))
-                local ending = data:reverse():find(ch(types.ee)) --last index of closetbl
+                local ending = data:reverse():find(ch(info.ee)) --last index of closetbl
                 assert(ending, "Unclosed table for key: "..key)
                 ending = #data - ending
                 local newtblstr = data:sub(_end+1, ending)
-                if #newtblstr ~= 0 then
+                --if #newtblstr ~= 0 then
                     local value = decode(newtblstr, true)
                     new[
                         tonumber(key) and tonumber(key) or key
                         ] = value
-                end
+                --end
             else
                 error"Invalid decode format."
             end
@@ -215,24 +293,26 @@ local tableencode,tabledecode = (function()
         return new
     end
     local function encode(tbl, nocycles, recursed)
-        recursed = (recursed or 0) + 1
+        recursed = (recursed or -1) + 1
         nocycles = nocycles or {}
         local str = ""
         local ctr = 0
         for i,v in next, tbl do
-            if ctr > 0 then str = str..ch(types.c) end
+            if ctr > 0 then str = str..comma end
             local type = type(v)
-            type = (type == 'number' and 'n') or (type == 'string' and 's') or (type == 'table' and 'e') or error"unsupported type: "..type
+            type = typetable[type] or error("unsupported type: "..type)
             if type ~= 'e' then
-                str = str..ch(types[type])
-                ..to(i)..ch(types.k)
-                ..to(v)..ch(types.ea)--..ch(types.c)
+                str = str..ch(info[type])
+                ..to(i)..ch(info.k)
+                ..to(v)..ch(info.ea)--..ch(info.c)
             else
+                --print()
                 assert(not tf(nocycles, v), "Cyclic table detected.")
+                
                 local data = encode(v, nocycles, recursed)
                 table.insert(nocycles, v) --no cyclic detection = stack overflow
-                str = str..ch(types[type])..to(i)..ch(types.k)
-                ..data..ch(types.ee)
+                local newentry = ch(info.e)..to(i)..ch(info.k)..(data or "")..ch(info.ee)
+                str = str..newentry
             end
     --append to old:   [datatype],      [keyname],   [key-value separator], [value], [END OF READ], [comma]
             ctr = ctr + 1
